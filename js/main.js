8bfc970c1b8de4705c8ad0b4ab89dd3aa0857581
Michese <michese48@mail.ru>
2020-05-21 21:23:07 +0300

    first commit

--- js/main.js
+++ js/main.js
@@ -1,74 +1,110 @@
-class ProductList {
-  constructor(container = '.products') {
-    this.container = container;
-    this.goods = [];
-    this.allProducts = [];
-    this._fetchProducts();
-    this._render();
-    this.priceAllGoods = this._sumPriceGoods();
-  }
+'use strict';
 
-  _fetchProducts() {
-    this.goods = [
-      {id: 1, title: 'Notebook', price: 20000},
-      {id: 2, title: 'Mouse', price: 1500},
-      {id: 3, title: 'Keyboard', price: 5000},
-      {id: 4, title: 'Gamepad', price: 4500},
-    ]
-  }
+const API = 'https://raw.githubusercontent.com/GeekBrainsTutorial/online-store-api/master/responses';
 
-  _render() {
-    const block = document.querySelector(this.container);
-
-    for (let product of this.goods) {
-      const productObject = new ProductItem(product);
-      this.allProducts.push(productObject);
-      block.insertAdjacentHTML('beforeend', productObject.render());
-    }
-  }
-
-  _sumPriceGoods() {
-    let result = 0;
-    this.allProducts.forEach(product => {
-      result += product.price;
+const getRequest = (url) => {
+    return new Promise((resolve, reject) => {
+        let xhr = new XMLHttpRequest();
+        xhr.open('GET', url, true);
+        xhr.onreadystatechange = () => {
+            if (xhr.readyState !== 4) return;
+            if (xhr.status !== 200) {
+                return reject(`error ${xhr.status} ${xhr.statusText}`);
+            } else {
+                return resolve(xhr.responseText);
+            }
+        }
+        xhr.send();
     })
-    return result;
-  }
+}
+
+class ProductList {
+    constructor(container = '.products') {
+        this.container = container;
+        this.goods = [];
+        this.allProducts = [];
+        // this._fetchProducts();
+        this._getProducts()
+            .then(data => {
+                this.goods = [...data];
+                this._render();
+            });
+    }
+
+    _fetchProducts() {
+        getRequest(`${API}/catalogData.json`).then(data => {
+            this.goods = JSON.parse(data);
+            this._render();
+        }).catch(error => console.log(error));
+    }
+
+    _getProducts() {
+        fetch(`${API}/catalogData.json`)
+            .then(response => response.json())
+            .catch(error => console.log(error));
+    }
+
+    _render() {
+        const block = document.querySelector(this.container);
+
+        for (let product of this.goods) {
+            const productObject = new ProductItem(product);
+            this.allProducts.push(productObject);
+            block.insertAdjacentHTML('beforeend', productObject.render());
+        }
+    }
+
+    _sumPriceGoods() {
+        let result = 0;
+        this.allProducts.forEach(product => {
+            result += product.price;
+        })
+        return result;
+    }
 }
 
 class ProductItem {
 
-  constructor(product, img = 'https://placehold.it/200x150') {
-    this.title = product.title;
-    this.price = product.price;
-    this.id = product.id;
-    this.img = img;
-  }
+    constructor(product, img = 'https://placehold.it/200x150') {
+        this.id_product = product.id_product;
+        this.product_name = product.product_name;
+        this.price = product.price;
+        this.img = img;
+    }
 
-  render() {
-    return `<div class="product-item" data-id="${this.id}">
+    render() {
+        return `<div class="product-item" data-id="${this.id}">
                 <img src="${this.img}" alt="Some img">
                 <div class="desc">
-                    <h3>${this.title}</h3>
+                    <h3>${this.product_name}</h3>
                     <p>${this.price} \u20bd</p>
                     <button class="buy-btn">Купить</button>
                 </div>
             </div>`;
-  }
+    }
 }
 
 class CartList {
-  constructor() {
-  }
-  // Добавить элемент в корзину
-  // Удалить элемент из корзины
-  // отрисовать элементы
+    constructor() {
+
+    }
+
 }
 
-class CartItem {
-  constructor() {
-  }
-  // вернуть разметку тегов HTML
+class CartItem extends ProductItem {
+    constructor(productItem, img) {
+        super(productItem, img)
+        this.quantity = 1;
+    }
+
+    render() {
+        return `<tr>
+            <td>${this.id_product}</td>
+            <td>${this.product_name}</td>
+            <td>${this.price}</td>
+            <td>${this.quantity}</td>
+        </tr>`;
+    }
 }
 
 new ProductList();
